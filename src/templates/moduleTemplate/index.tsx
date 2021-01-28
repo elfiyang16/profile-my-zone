9b79287215d6cb4af6a78c1e2466ef0f856690c7
Elfi Yang <elfiyang16@gmail.com>
2021-01-28 22:21:15 +0000

    feat: add moduleTemplate graphql query

---
+++ src/templates/moduleTemplate/index.tsx
@@ -0,0 +1,71 @@
+import { graphql } from "gatsby"
+
+// import ModuleTemplate from "./ModuleTemplate"
+
+export const pageQuery = graphql`
+  query($id: String!) {
+    contentfulModuleTemplate(id: { eq: $id }) {
+      id
+      pageTitle
+      urlPath
+      structuredData {
+        internal {
+          type
+          content
+        }
+      }
+      noIndex: doNotIndex
+      hero {
+        ...hero
+      }
+      blogs {
+        __typename
+        ... on Node {
+          ...blog
+        }
+      }
+    }
+  }
+
+  fragment hero on ContentfulHero {
+    id
+    colorTheme
+    heading1
+    heading2
+    image {
+      title
+      file {
+        contentType
+        fileName
+        url
+      }
+    }
+    entryTitle
+  }
+
+  fragment blog on ContentfulBlogPost {
+    id
+    author {
+      name
+    }
+    body {
+      raw
+    }
+    publishDate
+    slug
+    title
+    description {
+      description
+    }
+    heroImage {
+      title
+      file {
+        contentType
+        fileName
+        url
+      }
+    }
+  }
+`
+
+export default ModularTemplate
